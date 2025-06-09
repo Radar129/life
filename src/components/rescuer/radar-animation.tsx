ad814c4db09002c9be58075e0e2b9f5c1f7a4c52
radar <projectradar07@gmail.com>
2025-06-09 18:58:19 +0000

    cant see that visually

--- src/components/rescuer/radar-animation.tsx
+++ src/components/rescuer/radar-animation.tsx
@@ -12,20 +12,20 @@ export function RadarAnimation() {
       aria-label="Radar scan in progress"
     >
       {/* Static Background Grid/Circles */}
-      <div className="absolute inset-0 border border-primary/30 rounded-full"></div>
-      <div className="absolute inset-[25%] border border-primary/30 rounded-full"></div>
-      <div className="absolute inset-[50%] border border-primary/30 rounded-full"></div>
-      <div className="absolute inset-[75%] border border-primary/30 rounded-full"></div>
+      <div className="absolute inset-0 border-2 border-primary/60 rounded-full"></div>
+      <div className="absolute inset-[25%] border-2 border-primary/60 rounded-full"></div>
+      <div className="absolute inset-[50%] border-2 border-primary/60 rounded-full"></div>
+      <div className="absolute inset-[75%] border-2 border-primary/60 rounded-full"></div>
 
       {/* Optional: crosshair lines */}
-      <div className="absolute w-full h-[1px] bg-primary/20 top-1/2 -translate-y-1/2"></div>
-      <div className="absolute h-full w-[1px] bg-primary/20 left-1/2 -translate-x-1/2"></div>
+      <div className="absolute w-full h-[2px] bg-primary/50 top-1/2 -translate-y-1/2"></div>
+      <div className="absolute h-full w-[2px] bg-primary/50 left-1/2 -translate-x-1/2"></div>
       
       {/* Sweeping Line */}
       <div className="absolute w-full h-full animate-[spin_3s_linear_infinite]">
         {/* The arm of the radar */}
         <div 
-          className="absolute top-1/2 left-1/2 w-1/2 h-[2px] bg-gradient-to-r from-primary/10 via-primary/80 to-primary "
+          className="absolute top-1/2 left-1/2 w-1/2 h-[3px] bg-gradient-to-r from-transparent via-primary/60 to-primary"
           style={{ 
             transformOrigin: '0% 50%', // Rotate around the start of the line (center of radar)
             transform: 'translateY(-50%) rotate(0deg)', // Initial position
